31dfbef40c8359d686e72b544e456c2c39e3ba37
iyinraphael <iyinraphael@gmail.com>
2020-04-21 21:38:22 -0700

    addition and step one

---
+++ CardGame.playground/Pages/Solution.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+//: [Previous](@previous)
+
+import Foundation
+
+enum Card: Int {
+    case ace = 1
+    case two
+    case three
+    case four
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+    case ten
+    case jack
+    case queen
+    case king
+}
+
+
+extension Card: CustomStringConvertible {
+    var description: String {
+        return 
+    }
+}
+//: [Next](@next)
---
+++ Game-Card/Game-Card/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game-Card
+//
+//  Created by Iyin Raphael on 4/21/20.
+//  Copyright © 2020 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
